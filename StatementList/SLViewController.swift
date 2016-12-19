0481f7bec3cc6fbda129bd872b0d8ef5d1931bd4
Shelton Han <shelton@williamhill.com.au>
2016-12-20 09:26:42 +1100

    Minor UI adjustments, fix font size

--- StatementList/SLViewController.swift
+++ StatementList/SLViewController.swift
@@ -14,22 +14,13 @@ struct UrlPaths {
     static let dataUrl = "https://www.dropbox.com/s/tewg9b71x0wrou9/data.json?dl=1"
 }
 
-struct ThemeColors {
-    struct Background {
-        static let blue = UIColor(red:146/255, green:176/255, blue:176/255, alpha:1.0)
-        static let grey = UIColor(red:246/255, green:246/255, blue:246/255, alpha:1.0)
-        static let yellow = UIColor(red:255/255, green:204/255, blue:0.0, alpha:1.0)
-    }
-    struct Text {
-        static let grey = UIColor(red:138/255, green:138/255, blue:138/255, alpha:1.0)
-        static let black = UIColor(red:35/255, green:31/255, blue:32/255, alpha:1.0)
-    }
+struct TextColors {
+    static let black = UIColor(red:35/255, green:31/255, blue:32/255, alpha:1.0)
 }
 
-struct ThemeFont {
-    static let heading = UIFont(name: "HelveticaNeue-Light", size: 18)
-    static let normalText = UIFont(name: "HelveticaNeue-Light", size: 16)
-    static let boldText = UIFont(name: "HelveticaNeue-Bold", size: 16)
+struct TextFonts {
+    static let regularText = UIFont(name: "HelveticaNeue", size: 12)
+    static let boldText = UIFont(name: "HelveticaNeue-Bold", size: 12)
 }
 
 class SLViewController: UIViewController {
