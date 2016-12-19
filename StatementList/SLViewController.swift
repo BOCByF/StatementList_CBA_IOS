65dc1106288c53d464b520bc2c6ec7d2afb4ce4e
Shelton Han <shelton@williamhill.com.au>
2016-12-20 08:28:43 +1100

    Add StatementHeaderCell UI

--- StatementList/SLViewController.swift
+++ StatementList/SLViewController.swift
@@ -7,6 +7,30 @@
 //
 
 import UIKit
+import Alamofire
+
+
+struct UrlPaths {
+    static let dataUrl = "https://www.dropbox.com/s/tewg9b71x0wrou9/data.json?dl=1"
+}
+
+struct ThemeColors {
+    struct Background {
+        static let blue = UIColor(red:146/255, green:176/255, blue:176/255, alpha:1.0)
+        static let grey = UIColor(red:246/255, green:246/255, blue:246/255, alpha:1.0)
+        static let yellow = UIColor(red:255/255, green:204/255, blue:0.0, alpha:1.0)
+    }
+    struct Text {
+        static let grey = UIColor(red:138/255, green:138/255, blue:138/255, alpha:1.0)
+        static let black = UIColor(red:35/255, green:31/255, blue:32/255, alpha:1.0)
+    }
+}
+
+struct ThemeFont {
+    static let heading = UIFont(name: "HelveticaNeue-Light", size: 18)
+    static let normalText = UIFont(name: "HelveticaNeue-Light", size: 16)
+    static let boldText = UIFont(name: "HelveticaNeue-Bold", size: 16)
+}
 
 class SLViewController: UIViewController {
 
@@ -14,10 +38,13 @@ class SLViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        Alamofire.request(UrlPaths.dataUrl).responseJSON { response in
+            if let data = response.result.value {
+                print("data: \(data)")
+            }
+        }
     }
 
 
